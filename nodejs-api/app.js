86a5b0cbef56b1f0a95b0e1d86b52d7d83cf7176
Dissentleviathan <maghasp22@gmail.com>
2022-12-08 08:15:58 +0700

    datau

--- nodejs-api/app.js
+++ nodejs-api/app.js
@@ -1,25 +1,38 @@
-// (1) 
+// (1) definisikan module, middleware
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv/config')
 
-// (3)
+// (6) middleware body-parser
+// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(cors())
+
+// (7) import routes
+const mahasiswaRoutes = require('./routes/mahasiswa')
+
+// (8) app.use (mendaftarkan middleware baru ke Express)
+app.use('/mahasiswa', mahasiswaRoutes)
+
+// (3) koneksi ke database mongodb
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
 let db = mongoose.connection
 
-    // jika error
+    // handle error
     db.on('error', console.error.bind(console, 'Error Establishing a Database Connection?'))
-
-    // jika success
+    // handle success
     db.once('open', () => {
-       console.log('Database is connected');
+        console.log('Database is connected')
     })
 
-// (2) 
+// (2) listen port, dan buat callback dengan output console.log
 app.listen(process.env.PORT, () => {
-    
     console.log(`Server running on ${process.env.PORT}`);
-
 })
